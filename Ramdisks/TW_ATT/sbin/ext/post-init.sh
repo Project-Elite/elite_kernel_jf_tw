3753dfc11913175faf9b0fa8028036b0a9be6497
buckmarble <forrestmarble@gmail.com>
2013-07-21 11:23:58 -0400

    update ramdisks

--- Ramdisks/TW_ATT/sbin/ext/post-init.sh
+++ Ramdisks/TW_ATT/sbin/ext/post-init.sh
@@ -1,32 +1,31 @@
 #!/sbin/busybox sh
 #
 #Set Max Mhz for GPU
-echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 400000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set Max Mhz speed and booted flag to set Super Max
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
 
 # Remount FileSys RW
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 
 ## Create the kernel data directory
-if [ ! -d /data/.ktoonsez ];
+if [ ! -d /data/.elite ];
 then
-  mkdir /data/.ktoonsez
-  chmod 777 /data/.ktoonsez
+  mkdir /data/.elite
+  chmod 777 /data/.elite
 fi
 
 ## Enable "post-init" ...
-if [ -f /data/.ktoonsez/post-init.log ];
+if [ -f /data/.elite/post-init.log ];
 then
   # BackUp old post-init log
-  mv /data/.ktoonsez/post-init.log /data/.ktoonsez/post-init.log.BAK
+  mv /data/.elite/post-init.log /data/.elite/post-init.log.BAK
 fi
 
 # Start logging
-date >/data/.ktoonsez/post-init.log
-exec >>/data/.ktoonsez/post-init.log 2>&1
+date >/data/.elite/post-init.log
+exec >>/data/.elite/post-init.log 2>&1
 
 echo "Running Post-Init Script"
 
