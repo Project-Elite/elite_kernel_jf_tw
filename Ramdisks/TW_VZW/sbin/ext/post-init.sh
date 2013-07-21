81c5bf837359da1655f0a609f7e9dd74d3985eb9
buckmarble <forrestmarble@gmail.com>
2013-07-20 21:40:12 -0400

    fix build and update ramdisks

--- Ramdisks/TW_VZW/sbin/ext/post-init.sh
+++ Ramdisks/TW_VZW/sbin/ext/post-init.sh
@@ -1,11 +1,10 @@
 #!/sbin/busybox sh
 #
 #Set Max Mhz for GPU
-echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 400000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set Max Mhz speed and booted flag to set Super Max
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
 
 # Remount FileSys RW
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
--- Ramdisks/sbin/post-init.sh
+++ Ramdisks/sbin/post-init.sh
@@ -1,10 +1,11 @@
 #!/sbin/busybox sh
 #
 #Set Max Mhz for GPU
-echo 400000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set Max Mhz speed and booted flag to set Super Max
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
 
 # Remount FileSys RW
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
@@ -31,24 +32,15 @@ echo "Running Post-Init Script"
 
 ## Testing: Check for ExFat SD Card
 #
-SDTYPE1=`blkid /dev/block/mmcblk1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
-SDTYPE2=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
+SDTYPE=`blkid /dev/block/mmcblk1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
 
-if [ "${SDTYPE1}" == "exfat" ];
+if [ ${SDTYPE} == "exfat" ];
 then
   echo "ExFat-Debug: SD-Card is type ExFAT"
   echo "ExFat-Debug: trying to mount via fuse"
-  mount.exfat-fuse /dev/block/mmcblk1 /storage/extSdCard
+  ./mount.exfat-fuse /dev/block/mmcblk1 /storage/extSdCard
 else
-  echo "ExFat-Debug: SD-Card is type1: ${SDTYPE1}"
-fi
-if [ "${SDTYPE2}" == "exfat" ];
-then
-  echo "ExFat-Debug: SD-Card is type ExFAT"
-  echo "ExFat-Debug: trying to mount via fuse"
-  mount.exfat-fuse /dev/block/mmcblk1p1 /storage/extSdCard
-else
-  echo "ExFat-Debug: SD-Card is type2: ${SDTYPE2}"
+  echo "ExFat-Debug: SD-Card is type: ${SDTYPE}"
 fi
 
 # Remount FileSys RO
